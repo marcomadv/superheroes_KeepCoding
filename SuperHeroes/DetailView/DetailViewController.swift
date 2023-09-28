3f427534283f4f564f13126c7fed74178ab4645b
marcomadv <imarcoma@gmail.com>
2023-09-28 13:35:26 +0200

    Commit final

--- SuperHeroes/DetailView/DetailViewController.swift
+++ SuperHeroes/DetailView/DetailViewController.swift
@@ -27,7 +27,6 @@ class DetailViewController: UIViewController {
         self.data = data
         super.init(nibName: nil, bundle: nil)
     }
-    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
